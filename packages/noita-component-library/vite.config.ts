5e588514e1e017f2067ba42406c530dcd324fd4c
Gergo Nador <wotng2002@gmail.com>
2024-12-11 23:55:58 +0100

    setting up the noita component library

--- packages/noita-component-library/vite.config.ts
+++ packages/noita-component-library/vite.config.ts
@@ -1,38 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ['lib'], tsconfigPath: 'tsconfig.build.json' }),
+  ],
   build: {
     lib: {
-      entry: resolve("lib/main.ts"),
-      formats: ["es"],
+      entry: resolve('lib/main.tsx'),
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: ["lib/**/*.d.ts"],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ]),
-      ),
+      external: ['react', 'react/jsx-runtime'],
       output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
       },
     },
   },
