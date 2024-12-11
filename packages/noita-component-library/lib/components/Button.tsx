8abe8df9aa7cc416780d2ca119a9e2636f65fbf1
Gergo Nador <wotng2002@gmail.com>
2024-12-11 21:02:46 +0100

    adding empty library

--- packages/noita-component-library/lib/components/Button.tsx
+++ packages/noita-component-library/lib/components/Button.tsx
@@ -1,11 +1,11 @@
-import css from './Button.module.css';
-import arrowLeft from '../../../assets/icons/keyboard_cursor_left_active.png';
-import arrowRight from '../../../assets/icons/keyboard_cursor_right_active.png';
-import React, { useRef } from 'react';
+import css from "./Button.module.css";
+import arrowLeft from "../../../assets/icons/keyboard_cursor_left_active.png";
+import arrowRight from "../../../assets/icons/keyboard_cursor_right_active.png";
+import React, { MutableRefObject, useRef } from "react";
 
 interface ButtonProps {
   children?: string | React.ReactNode | React.ReactNode[];
-  decoration?: 'left' | 'right' | 'both';
+  decoration?: "left" | "right" | "both";
   disabled?: boolean;
   onClick?: () => void;
   onDisabledClick?: () => void;
@@ -24,54 +24,54 @@ export const Button = ({
   const buttonOnClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
     const isMouseClick = e.detail > 0;
     if (isMouseClick) {
-      buttonRef.current.blur();
+      buttonRef.current?.blur();
     }
 
-    if (disabled && typeof onDisabledClick === 'function') {
+    if (disabled && typeof onDisabledClick === "function") {
       onDisabledClick();
       return;
     }
 
     if (disabled) return;
 
-    if (typeof onClick === 'function') {
+    if (typeof onClick === "function") {
       onClick();
     } else {
-      console.error('button not implemented');
+      console.error("button not implemented");
     }
   };
 
   const leftButton =
-    decoration === 'left' || decoration === 'both' || decoration === undefined;
-  const rightButton = decoration === 'right' || decoration === 'both';
+    decoration === "left" || decoration === "both" || decoration === undefined;
+  const rightButton = decoration === "right" || decoration === "both";
 
   return (
-    <div className={css['container']}>
+    <div className={css["container"]}>
       <button
-        className={css['button-content']}
+        className={css["button-content"]}
         onClick={buttonOnClick}
-        ref={buttonRef}
+        ref={buttonRef as MutableRefObject<HTMLButtonElement>}
         style={{
-          cursor: disabled ? 'not-allowed' : 'pointer',
+          cursor: disabled ? "not-allowed" : "pointer",
         }}
       >
         {leftButton && (
           <span
-            className={`${css['icon-container']} ${css['icon-container-left']}`}
+            className={`${css["icon-container"]} ${css["icon-container-left"]}`}
           >
             <img
               src={arrowLeft}
               height={iconSize * 2}
               width={iconSize}
-              className={css['icon']}
-              alt={'Button Arrow'}
+              className={css["icon"]}
+              alt={"Button Arrow"}
             />
           </span>
         )}
 
         <span
           style={{
-            color: disabled ? '#FFFFFFAA' : 'inherit',
+            color: disabled ? "#FFFFFFAA" : "inherit",
           }}
         >
           {children}
@@ -79,14 +79,14 @@ export const Button = ({
 
         {rightButton && (
           <span
-            className={`${css['icon-container']} ${css['icon-container-right']}`}
+            className={`${css["icon-container"]} ${css["icon-container-right"]}`}
           >
             <img
               src={arrowRight}
               height={iconSize * 2}
               width={iconSize}
-              className={css['icon']}
-              alt={'Button Arrow'}
+              className={css["icon"]}
+              alt={"Button Arrow"}
             />
           </span>
         )}
