1705ad3266dc36ae33529c58e296be759f27dcbb
Gergo Nador <wotng2002@gmail.com>
2025-06-01 21:42:35 +0200

    adding some stuff to progress tracker secrets

--- apps/web/src/pages/progress/noita-progress-tracker-secrets.tsx
+++ apps/web/src/pages/progress/noita-progress-tracker-secrets.tsx
@@ -1,3 +1,22 @@
+import { useSave00Store } from '../../stores/save00.ts';
+import { Flex } from '../../components/flex.tsx';
+
 export const NoitaProgressTrackerSecrets = () => {
-  return <div>Secrets</div>;
+  const { unlockedOrbs } = useSave00Store();
+
+  return (
+    <div>
+      <h1>Secrets</h1>
+      <h2>Unlocked Orbs</h2>
+      <div>
+        <Flex gap={5}>{unlockedOrbs?.map((o) => <span>{o}</span>)}</Flex>
+      </div>
+      <h2>Decoration</h2>
+      <div>
+        <div>Golden necklace</div>
+        <div>Amulet</div>
+        <div>Crown</div>
+      </div>
+    </div>
+  );
 };
