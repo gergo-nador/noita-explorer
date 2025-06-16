4cbbc0abaafa515855082b0b862738f71ff1d374
Gergo Nador <wotng2002@gmail.com>
2025-06-16 09:53:47 +0200

    main.tsx refactor

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,32 +1,63 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, RootOptions } from 'react-dom/client';
 import './index.css';
 import { App } from './app.tsx';
-import * as Sentry from '@sentry/react';
 import { environment } from './environment.ts';
 
 // TODO: be able to enable this in the settings
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  sendDefaultPii: true,
-  debug: true,
-  environment: environment,
-});
+let rootErrorHandling: RootOptions = {
+  onUncaughtError: (
+    error: unknown,
+    errorInfo: { componentStack?: string | undefined },
+  ) => {
+    console.error('Uncaught error: ', error, errorInfo);
+  },
+};
 
-const container = document.getElementById('root')!;
-const root = createRoot(container, {
-  // Callback called when an error is thrown and not caught by an Error Boundary.
-  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
-    console.warn('Uncaught error', error, errorInfo.componentStack);
-  }),
-  // Callback called when React catches an error in an Error Boundary.
-  onCaughtError: Sentry.reactErrorHandler(),
-  // Callback called when React automatically recovers from errors.
-  onRecoverableError: Sentry.reactErrorHandler(),
-});
+const val = 1;
+if (val === 1) {
+  import('@sentry/react')
+    .then(
+      (Sentry) => {
+        Sentry.init({
+          dsn: import.meta.env.VITE_SENTRY_DSN,
+          sendDefaultPii: true,
+          debug: true,
+          environment: environment,
+        });
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+        rootErrorHandling = {
+          // Callback called when an error is thrown and not caught by an Error Boundary.
+          onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+            console.warn('Uncaught error', error, errorInfo.componentStack);
+          }),
+          // Callback called when React catches an error in an Error Boundary.
+          onCaughtError: Sentry.reactErrorHandler(),
+          // Callback called when React automatically recovers from errors.
+          onRecoverableError: Sentry.reactErrorHandler(),
+        };
+      },
+      () => {
+        console.error('Failed to import Sentry');
+      },
+    )
+    .catch(() => {
+      console.error('Failed to initialize Sentry');
+    })
+    .then(() => {
+      startApp();
+    });
+} else {
+  startApp();
+}
+
+function startApp() {
+  const container = document.getElementById('root')!;
+  const root = createRoot(container, rootErrorHandling);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
