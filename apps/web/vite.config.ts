3b05e3208745034b0176f1a952ddd0fb2297365f
Gergo Nador <wotng2002@gmail.com>
2025-06-16 21:19:34 +0200

    disabling sentry source map upload when auth token is not present

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,25 +1,32 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    mkcert(),
-    sentryVitePlugin({
-      org: 'noita-explorer',
-      project: 'javascript-react',
-      telemetry: true,
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-  server: {
-    port: 4000,
-  },
+  return {
+    plugins: [
+      react(),
+      mkcert(),
+      sentryVitePlugin({
+        org: 'noita-explorer',
+        project: 'javascript-react',
+        telemetry: true,
+        // don't upload source maps when the sentry auth token is not present
+        disable: !env.VITE_SENTRY_AUTH_TOKEN,
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+      }),
+    ],
 
-  build: {
-    sourcemap: true,
-  },
+    server: {
+      port: 4000,
+    },
+
+    build: {
+      sourcemap: true,
+    },
+  };
 });
