37010ba67863e52929562a9177873e95894d59f8
Gergo Nador <wotng2002@gmail.com>
2025-07-02 00:18:53 +0200

    generate static assets for spells and enemies

--- apps/web/scripts/create-seo-sites-for-wiki/generate-html.ts
+++ apps/web/scripts/create-seo-sites-for-wiki/generate-html.ts
@@ -10,38 +10,46 @@ interface Props {
     height: string;
     alt: string;
   };
+  redirectUrl: string;
 }
 
 // mom can we have nextjs at home? nextjs at home:
-export const generateHtmlHead = ({
+export const generateHtml = ({
   title,
   description,
   url,
   siteName,
   image,
+  redirectUrl,
 }: Props) => {
   // no but for real, I just don't want to migrate the whole application
   // just so the wiki page can have some SEO
 
-  return `<head>
-  <title>${title}</title>
-  <meta name="description" content="${description}">
-  <link rel="canonical" href="${url}">
-  <meta property="og:title" content="${title}">
-  <meta property="og:description" content="${description}">
-  <meta property="og:image" content="${image.url}">
-  <meta property="og:image:url" content="${image.url}">
-  <meta property="og:image:type" content="${image.mimeType}">
-  <meta property="og:image:width" content="${image.width}">
-  <meta property="og:image:height" content="${image.height}">
-  <meta property="og:image:alt" content="${image.alt}">
-  <meta property="og:url" content="${url}">
-  <meta property="og:type" content="article">
-  <meta property="og:site_name" content="${siteName}">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="${title}">
-  <meta name="twitter:description" content="${description}">
-  <meta name="twitter:image" content="${image.url}">
-  <meta name="twitter:site" content="@mydevblog">
-</head>`;
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>${title}</title>
+    <meta name="description" content="${description}">
+    <link rel="canonical" href="${url}">
+    <meta property="og:title" content="${title}">
+    <meta property="og:description" content="${description}">
+    <meta property="og:image" content="${image.url}">
+    <meta property="og:image:url" content="${image.url}">
+    <meta property="og:image:type" content="${image.mimeType}">
+    <meta property="og:image:width" content="${image.width}">
+    <meta property="og:image:height" content="${image.height}">
+    <meta property="og:image:alt" content="${image.alt}">
+    <meta property="og:url" content="${url}">
+    <meta property="og:type" content="article">
+    <meta property="og:site_name" content="${siteName}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="${title}">
+    <meta name="twitter:description" content="${description}">
+    <meta name="twitter:image" content="${image.url}">
+  </head>
+  <body>
+    <script>window.location.replace('${redirectUrl}')</script>
+  </body>
+</html>`;
 };
--- apps/web/scripts/create-seo-sites-for-wiki/index.ts
+++ apps/web/scripts/create-seo-sites-for-wiki/index.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
 import { NoitaWakData } from '@noita-explorer/model-noita';
 import { Buffer } from 'buffer';
-import { promiseHelper } from '@noita-explorer/tools';
-import { generateHtmlHead } from './generate-html';
+import { generateHtml } from './generate-html';
 import { deployUrls } from '../../src/deployUrls';
 import * as dotenv from 'dotenv';
 import * as path from 'node:path';
@@ -53,58 +52,145 @@ function readNoitaWakData() {
 }
 
 async function generateStaticAssets(data: NoitaWakData) {
-  await promiseHelper.fromCallbackProvider((callback) =>
-    fs.mkdir('public/g', callback),
-  );
+  const fsPath = 'public/g/wiki';
+  const webPath = '/g/wiki';
+  fs.mkdirSync(fsPath, { recursive: true });
 
   // perks
-  const perksFolderPath = 'public/g/perks';
-  const perksPublicImagePath = '/g/perks';
-  await promiseHelper.fromCallbackProvider((callback) =>
-    fs.mkdir(perksFolderPath, callback),
-  );
+  {
+    const perksFolderPath = fsPath + '/perks';
+    const perksPublicImagePath = webPath + '/perks';
+    fs.mkdirSync(perksFolderPath, { recursive: true });
 
-  for (const perk of data.perks) {
-    const redirectUrl = `/wiki/perks?perk=${perk.id}`;
-    const imageWebPath = `${perksPublicImagePath}/${perk.id}.png`;
-    const imagePath = `${perksFolderPath}/${perk.id}.png`;
-    const imagePathFs = path.resolve(imagePath);
+    for (const perk of data.perks) {
+      const redirectUrl = `/wiki/perks?perk=${perk.id}`;
+      const imageWebPath = `${perksPublicImagePath}/${perk.id}.png`;
+      const imagePath = `${perksFolderPath}/${perk.id}.png`;
+      const imagePathFs = path.resolve(imagePath);
 
-    const width = 1500;
-    const height = 1500;
+      const width = 1500;
+      const height = 1500;
 
-    await generateImage({
-      base64: perk.imageBase64,
-      outputPath: imagePathFs,
-      width: width,
-      height: height,
-    });
+      if (!fs.existsSync(imagePathFs)) {
+        await generateImage({
+          base64: perk.imageBase64,
+          outputPath: imagePathFs,
+          width: width,
+          height: height,
+        });
+      }
 
-    const htmlHead = generateHtmlHead({
-      siteName: 'Noita Explorer',
-      title: perk.name,
-      description: perk.description,
-      url: deployUrls.noitaExplorer.production + redirectUrl,
-      image: {
-        url: imageWebPath,
-        mimeType: 'image/png',
-        width: width.toString(),
-        height: height.toString(),
-        alt: perk.name,
-      },
-    });
+      const html = generateHtml({
+        siteName: 'Noita Explorer',
+        title: perk.name,
+        description: perk.description,
+        url: deployUrls.noitaExplorer.production + redirectUrl,
+        image: {
+          url: imageWebPath,
+          mimeType: 'image/png',
+          width: width.toString(),
+          height: height.toString(),
+          alt: perk.name,
+        },
 
-    const html = `<!DOCTYPE html>
-<html lang="en">
-  ${htmlHead}
-  <body>
-    <script>window.location.replace('${redirectUrl}')</script>
-  </body>
-</html>`;
+        redirectUrl: redirectUrl,
+      });
 
-    const buffer = Buffer.from(html);
-    fs.writeFileSync(`public/g/perks/${perk.id}.html`, buffer);
+      const buffer = Buffer.from(html);
+      fs.writeFileSync(`${perksFolderPath}/${perk.id}.html`, buffer);
+    }
+    console.log('Static assets generated - Perks');
   }
 
-  console.log('Generated static files successfully.');
+  // spells
+  {
+    const spellsFolderPath = fsPath + '/spells';
+    const spellsPublicImagePath = webPath + '/spells';
+    fs.mkdirSync(spellsFolderPath, { recursive: true });
+
+    for (const spell of data.spells) {
+      const redirectUrl = `/wiki/spells?spell=${spell.id}`;
+      const imageWebPath = `${spellsPublicImagePath}/${spell.id}.png`;
+      const imagePath = `${spellsFolderPath}/${spell.id}.png`;
+      const imagePathFs = path.resolve(imagePath);
+
+      const width = 1500;
+      const height = 1500;
+
+      if (!fs.existsSync(imagePathFs)) {
+        await generateImage({
+          base64: spell.imageBase64,
+          outputPath: imagePathFs,
+          width: width,
+          height: height,
+        });
+      }
+
+      const html = generateHtml({
+        siteName: 'Noita Explorer',
+        title: spell.name,
+        description: spell.description,
+        url: deployUrls.noitaExplorer.production + redirectUrl,
+        image: {
+          url: imageWebPath,
+          mimeType: 'image/png',
+          width: width.toString(),
+          height: height.toString(),
+          alt: spell.name,
+        },
+
+        redirectUrl: redirectUrl,
+      });
+
+      const buffer = Buffer.from(html);
+      fs.writeFileSync(`${spellsFolderPath}/${spell.id}.html`, buffer);
+    }
+    console.log('Static assets generated - Spells');
+  }
+
+  // enemies
+  {
+    const enemiesFolderPath = fsPath + '/enemies';
+    const enemiesPublicImagePath = webPath + '/enemies';
+    fs.mkdirSync(enemiesFolderPath, { recursive: true });
+
+    for (const enemy of data.enemies) {
+      const redirectUrl = `/wiki/enemies?enemy=${enemy.id}`;
+      const imageWebPath = `${enemiesPublicImagePath}/${enemy.id}.png`;
+      const imagePath = `${enemiesFolderPath}/${enemy.id}.png`;
+      const imagePathFs = path.resolve(imagePath);
+
+      const width = 1500;
+      const height = 1500;
+
+      if (!fs.existsSync(imagePathFs)) {
+        await generateImage({
+          base64: enemy.imageBase64,
+          outputPath: imagePathFs,
+          width: width,
+          height: height,
+        });
+      }
+
+      const html = generateHtml({
+        siteName: 'Noita Explorer',
+        title: enemy.name,
+        description: `${enemy.hp}❤️ ${enemy.hasGoldDrop ? enemy.goldDrop : '-'}💰`,
+        url: deployUrls.noitaExplorer.production + redirectUrl,
+        image: {
+          url: imageWebPath,
+          mimeType: 'image/png',
+          width: width.toString(),
+          height: height.toString(),
+          alt: enemy.name,
+        },
+
+        redirectUrl: redirectUrl,
+      });
+
+      const buffer = Buffer.from(html);
+      fs.writeFileSync(`${enemiesFolderPath}/${enemy.id}.html`, buffer);
+    }
+    console.log('Static assets generated - Enemies');
+  }
 }
