92b897e461d69def0b128914b8406b96977a8f66
Gergo Nador <wotng2002@gmail.com>
2025-07-12 17:35:52 +0200

    copy link improvements

--- apps/web/src/components/copy-link-text.tsx
+++ apps/web/src/components/copy-link-text.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Flex, HoveredStyle } from '@noita-explorer/react-utils';
 import linkIcon from '../assets/icons/link-icon.png';
 import { Icon, useToast } from '@noita-explorer/noita-component-library';
@@ -12,16 +12,32 @@ interface Props {
 
 export const CopyLinkText = ({ children, link, iconSize }: Props) => {
   const toast = useToast();
+  const [icon, setIcon] = useState<'link' | 'check' | 'error'>('link');
+  const timeoutRef = useRef<number>(undefined);
+
   const copyToClipboard = () =>
     noitaAPI.clipboard
       .set(link)
-      .then(() => toast.success('Link copied.'))
-      .catch(() => toast.error('Failed to copy link to clipboard.'));
+      .then(() => {
+        toast.success('Link copied.');
+        setIcon('check');
+        window.clearTimeout(timeoutRef.current);
+        timeoutRef.current = window.setTimeout(() => setIcon('link'), 2000);
+      })
+      .catch(() => {
+        toast.error('Failed to copy link to clipboard.');
+        setIcon('error');
+        window.clearTimeout(timeoutRef.current);
+        timeoutRef.current = window.setTimeout(() => setIcon('link'), 2000);
+      });
+
   return (
     <HoveredStyle style={{ textDecoration: 'underline', cursor: 'pointer' }}>
       <Flex onClick={() => copyToClipboard()} gap={8}>
         {children}
-        <Icon src={linkIcon} size={iconSize ?? 20} />
+        {icon === 'link' && <Icon src={linkIcon} size={iconSize ?? 20} />}
+        {icon === 'check' && <Icon type='check' size={iconSize ?? 20} />}
+        {icon === 'error' && <Icon type='cross' size={iconSize ?? 20} />}
       </Flex>
     </HoveredStyle>
   );
