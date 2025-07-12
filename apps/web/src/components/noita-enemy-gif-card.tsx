c2da2b29ec835b0587dfbd83406a4419290da23e
Gergo Nador <wotng2002@gmail.com>
2025-07-12 18:41:47 +0200

    gif tooltip when gif is hovered

--- apps/web/src/components/noita-enemy-gif-card.tsx
+++ apps/web/src/components/noita-enemy-gif-card.tsx
@@ -24,21 +24,21 @@ export const NoitaEnemyGifCard = ({ enemy, gif, width, height }: Props) => {
   const gifDisplayName = gif.name.replaceAll('_', ' ');
 
   return (
-    <Flex style={{ width: 'fit-content' }} column center>
-      <NoitaTooltipWrapper
-        content={
+    <NoitaTooltipWrapper
+      content={
+        <div>
           <div>
-            <div>
-              Loop: <BooleanIcon value={gif.loop} />
-            </div>
-            <div>Frames: {gif.frameCount}</div>
-            <div>
-              Dimensions: {gif.frameWidth} x {gif.frameHeight} pixels
-            </div>
-            <div>frame delay: {gif.frameWait * 1000}ms</div>
+            Loop: <BooleanIcon value={gif.loop} />
           </div>
-        }
-      >
+          <div>Frames: {gif.frameCount}</div>
+          <div>
+            Dimensions: {gif.frameWidth} x {gif.frameHeight} pixels
+          </div>
+          <div>frame delay: {gif.frameWait * 1000}ms</div>
+        </div>
+      }
+    >
+      <Flex style={{ width: 'fit-content' }} column center>
         <div style={{ maxWidth: '100%' }}>
           <span
             style={{
@@ -51,41 +51,42 @@ export const NoitaEnemyGifCard = ({ enemy, gif, width, height }: Props) => {
           </span>
           {gif.loop && <span style={{ fontSize: 10 }}> ∞</span>}
         </div>
-      </NoitaTooltipWrapper>
-      {gif.loop ? (
-        <div>
-          <PixelatedImage
-            src={`/g/enemy-gifs/${enemy.id}/${gif.name}.gif`}
-            alt={`${enemy.name} ${gif.name}`}
-            width={width}
-            height={height}
-          />
-        </div>
-      ) : (
-        <div
-          onClick={() => increaseGifRestartCounter()}
-          onMouseEnter={() => setShowGif(true)}
-          onMouseLeave={() => setShowGif(false)}
-        >
-          {showGif ? (
+
+        {gif.loop ? (
+          <div>
             <PixelatedImage
-              src={`/g/enemy-gifs/${enemy.id}/${gif.name}.gif?c=${gifRestartCounter}`}
+              src={`/g/enemy-gifs/${enemy.id}/${gif.name}.gif`}
               alt={`${enemy.name} ${gif.name}`}
               width={width}
               height={height}
-              onMouseEnter={() => increaseGifRestartCounter()}
             />
-          ) : (
-            <PixelatedImage
-              src={`/g/enemy-gifs/${enemy.id}/${gif.name}-f.png`}
-              alt={`${enemy.name} ${gif.name}`}
-              width={width}
-              height={height}
-              onMouseEnter={() => increaseGifRestartCounter()}
-            />
-          )}
-        </div>
-      )}
-    </Flex>
+          </div>
+        ) : (
+          <div
+            onClick={() => increaseGifRestartCounter()}
+            onMouseEnter={() => setShowGif(true)}
+            onMouseLeave={() => setShowGif(false)}
+          >
+            {showGif ? (
+              <PixelatedImage
+                src={`/g/enemy-gifs/${enemy.id}/${gif.name}.gif?c=${gifRestartCounter}`}
+                alt={`${enemy.name} ${gif.name}`}
+                width={width}
+                height={height}
+                onMouseEnter={() => increaseGifRestartCounter()}
+              />
+            ) : (
+              <PixelatedImage
+                src={`/g/enemy-gifs/${enemy.id}/${gif.name}-f.png`}
+                alt={`${enemy.name} ${gif.name}`}
+                width={width}
+                height={height}
+                onMouseEnter={() => increaseGifRestartCounter()}
+              />
+            )}
+          </div>
+        )}
+      </Flex>
+    </NoitaTooltipWrapper>
   );
 };
