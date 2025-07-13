9175dab69580d624b2f96e68826e7e542a84e4e0
Gergo Nador <wotng2002@gmail.com>
2025-07-13 07:22:39 +0200

    progress orbs

---
+++ apps/web/src/pages/progress/components/ProgressOrbs.tsx
@@ -0,0 +1,82 @@
+import { PixelatedImage } from '@noita-explorer/noita-component-library';
+import { enumerateHelpers } from '@noita-explorer/tools';
+import { useSave00Store } from '../../../stores/save00.ts';
+import { Flex } from '@noita-explorer/react-utils';
+
+export const ProgressOrbs = () => {
+  const { unlockedOrbs, flags } = useSave00Store();
+
+  const OrbGif = ({
+    src,
+  }: {
+    src: string | 'orb_red_evil' | 'orb_picked' | 'orb_discovered';
+  }) => {
+    return (
+      <PixelatedImage src={`/g/orb-gifs/${src}/default.gif`} height={120} />
+    );
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          maxWidth: '600px',
+        }}
+      >
+        {enumerateHelpers.range(0, 12).map((orbId) => {
+          const orbIdString = String(orbId).padStart(2, '0');
+          const hasEast = orbId < 11;
+          const hasWest = orbId < 11;
+
+          const progressFlagUnlocked = flags?.has(
+            'progress_orb_pickup_' + orbIdString,
+          );
+          const isOrbPickedUp = unlockedOrbs?.includes(String(orbId));
+          const isOrbWestPickedUp = unlockedOrbs?.includes(String(orbId + 128));
+          const isOrbEastPickedUp = unlockedOrbs?.includes(String(orbId + 256));
+
+          return (
+            <>
+              <Flex center column>
+                {hasWest && (
+                  <>
+                    {isOrbWestPickedUp ? (
+                      <OrbGif src='orb_picked' />
+                    ) : (
+                      <OrbGif src='orb_red_evil' />
+                    )}
+                    <div>Orb {orbId} West</div>
+                  </>
+                )}
+              </Flex>
+              <Flex center column>
+                {isOrbPickedUp ? (
+                  <OrbGif src='orb_picked' />
+                ) : progressFlagUnlocked ? (
+                  <OrbGif src='orb_discovered' />
+                ) : (
+                  <OrbGif src={'orb_' + orbIdString} />
+                )}
+                <div>Orb {orbId}</div>
+              </Flex>
+              <Flex center column>
+                {hasEast && (
+                  <>
+                    {isOrbEastPickedUp ? (
+                      <OrbGif src='orb_picked' />
+                    ) : (
+                      <OrbGif src='orb_red_evil' />
+                    )}
+                    <div>Orb {orbId} East</div>
+                  </>
+                )}
+              </Flex>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- apps/web/src/pages/progress/noita-progress-tracker-secrets.tsx
+++ apps/web/src/pages/progress/noita-progress-tracker-secrets.tsx
@@ -1,23 +1,17 @@
-import { useSave00Store } from '../../stores/save00.ts';
-import { Flex } from '@noita-explorer/react-utils';
 import { PlayerDecorations } from './components/player-decorations.tsx';
+import { Header } from '@noita-explorer/noita-component-library';
+import { ProgressOrbs } from './components/ProgressOrbs.tsx';
 
 export const NoitaProgressTrackerSecrets = () => {
-  const { unlockedOrbs } = useSave00Store();
-
   return (
     <div style={{ padding: 10 }}>
       <h1>Secrets</h1>
-      <h2>Unlocked Orbs</h2>
-      <div>
-        <Flex gap={5} wrap='wrap'>
-          {unlockedOrbs?.map((o) => (
-            <span key={o}>{o}</span>
-          ))}
-        </Flex>
-      </div>
-      <h2>Decoration</h2>
-      <PlayerDecorations />
+      <Header title='Decorations'>
+        <PlayerDecorations />
+      </Header>
+      <Header title='Orbs'>
+        <ProgressOrbs />
+      </Header>
     </div>
   );
 };
