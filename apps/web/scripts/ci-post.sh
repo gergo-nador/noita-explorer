abf9419f70766f8a265465896dd5f72253b5c861
Gergo Nador <wotng2002@gmail.com>
2025-07-14 23:50:30 +0200

    commit package.jsons

---
+++ apps/web/scripts/ci-post.sh
@@ -0,0 +1,9 @@
+# Sentry upload source maps
+if [ -z "$SENTRY_AUTH_TOKEN" ]; then
+    echo "SENTRY_AUTH_TOKEN is empty. Skipping source map upload."
+    exit 0
+fi
+
+# https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/cli/
+sentry-cli sourcemaps inject dist
+sentry-cli sourcemaps upload dist
