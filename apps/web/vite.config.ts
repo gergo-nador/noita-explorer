1bd84064d52ef5d09b31ad6c349c4b022eb57c43
Gergo Nador <wotng2002@gmail.com>
2025-07-14 19:32:45 +0200

    removed @sentry/vite-plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,41 +1,25 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig({
+  base: '/',
+  plugins: [react(), mkcert()],
 
-  return {
-    base: '/',
-    plugins: [
-      react(),
-      mkcert(),
-      sentryVitePlugin({
-        org: 'noita-explorer',
-        project: 'javascript-react',
-        telemetry: false,
-        // don't upload source maps when the sentry auth token is not present
-        disable: !env.VITE_SENTRY_AUTH_TOKEN,
-        authToken: env.VITE_SENTRY_AUTH_TOKEN,
-      }),
-    ],
+  server: {
+    port: 4000,
+  },
 
-    server: {
-      port: 4000,
-    },
-
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name][extname]',
-          entryFileNames: '[name].[format].js',
-        },
-        // exclude all
-        external: ['jimp', 'omggif', 'canvas', /images\.node-/, /gif\.node-/],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].[format].js',
       },
+      // exclude all
+      external: ['jimp', 'omggif', 'canvas', /images\.node-/, /gif\.node-/],
     },
-  };
+  },
 });
