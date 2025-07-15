41a99c217d8849cd8977bb8194ca621445b2ecdd
Gergo Nador <wotng2002@gmail.com>
2025-07-15 16:16:33 +0200

    delete source maps after sentry upload

--- apps/web/scripts/ci-post.sh
+++ apps/web/scripts/ci-post.sh
@@ -7,3 +7,7 @@ fi
 # https://docs.sentry.io/platforms/javascript/sourcemaps/uploading/cli/
 sentry-cli sourcemaps inject dist
 sentry-cli sourcemaps upload dist
+
+# delete sourcemaps, they just take up space
+rm ./dist/*.map
+rm ./dist/**/*.map
