cfb100d2fdc5948c50d04d63708c2690e16f12ce
Gergo Nador <wotng2002@gmail.com>
2025-08-03 12:00:54 +0200

    move protection assets to public

--- apps/web/src/noita/noita-protections.ts
+++ apps/web/src/noita/noita-protections.ts
@@ -1,102 +1,88 @@
 import { StringKeyDictionary } from '@noita-explorer/model';
 import { NoitaProtection } from '@noita-explorer/model-noita';
 
-import iconProtectionAll from '../assets/icons/protection/protection_all.png';
-import iconProtectionElectricity from '../assets/icons/protection/protection_electricity.png';
-import iconProtectionElectricityStun from '../assets/icons/protection/protection_electricity_stun.png';
-import iconProtectionExplosion from '../assets/icons/protection/protection_explosion.png';
-import iconProtectionFire from '../assets/icons/protection/protection_fire.png';
-import iconProtectionFreeze from '../assets/icons/protection/protection_freeze.png';
-import iconProtectionFreezeStun from '../assets/icons/protection/protection_freeze_stun.png';
-import iconProtectionGlue from '../assets/icons/protection/protection_glue.png';
-import iconProtectionMelee from '../assets/icons/protection/protection_melee.png';
-import iconProtectionPhysicsImpact from '../assets/icons/protection/protection_physics_impact.png';
-import iconProtectionPolymorph from '../assets/icons/protection/protection_polymorph.png';
-import iconProtectionProjectile from '../assets/icons/protection/protection_projectile.png';
-import iconProtectionRadioActivity from '../assets/icons/protection/protection_radioactivity.png';
-import iconProtectionResurrection from '../assets/icons/protection/protection_resurrection.png';
-import iconProtectionSuffocate from '../assets/icons/protection/protection_suffocate.png';
-import iconProtectionTouchMagic from '../assets/icons/protection/protection_touch_of.png';
+const basePath = '/images/data-wak/protections/';
+const getImagePath = (file: string) => basePath + file;
 
 export const NoitaProtections: StringKeyDictionary<NoitaProtection> = {
   PROTECTION_ALL: {
     id: 'PROTECTION_ALL',
     name: 'Immune to EVERYTHING',
-    image: iconProtectionAll,
+    image: getImagePath('protection_all.png'),
   },
   PROTECTION_ELECTRICITY: {
     id: 'PROTECTION_ELECTRICITY',
     name: 'Electricity Immunity',
-    image: iconProtectionElectricity,
+    image: getImagePath('protection_electricity.png'),
   },
   STUN_PROTECTION_ELECTRICITY: {
     id: 'STUN_PROTECTION_ELECTRICITY',
     name: 'Stun Electricity Immunity',
-    image: iconProtectionElectricityStun,
+    image: getImagePath('protection_electricity_stun.png'),
   },
   PROTECTION_EXPLOSION: {
     id: 'PROTECTION_EXPLOSION',
     name: 'Explosion Immunity',
-    image: iconProtectionExplosion,
+    image: getImagePath('protection_explosion.png'),
   },
   PROTECTION_FIRE: {
     id: 'PROTECTION_FIRE',
     name: 'Fire Immunity',
-    image: iconProtectionFire,
+    image: getImagePath('protection_fire.png'),
   },
   PROTECTION_FREEZE: {
     id: 'PROTECTION_FREEZE',
     name: 'Freeze Immunity',
-    image: iconProtectionFreeze,
+    image: getImagePath('protection_freeze.png'),
   },
   STUN_PROTECTION_FREEZE: {
     id: 'STUN_PROTECTION_FREEZE',
     name: 'Stun Freeze Immunity',
-    image: iconProtectionFreezeStun,
+    image: getImagePath('protection_freeze_stun.png'),
   },
   PROTECTION_GLUE: {
     id: 'PROTECTION_GLUE',
     name: 'Glue Immunity',
-    image: iconProtectionGlue,
+    image: getImagePath('protection_glue.png'),
   },
   PROTECTION_MELEE: {
     id: 'PROTECTION_MELEE',
     name: 'Melee Immunity',
-    image: iconProtectionMelee,
+    image: getImagePath('protection_melee.png'),
   },
   PROTECTION_PHYSICS_IMPACT: {
     id: 'PROTECTION_PHYSICS_IMPACT',
     name: 'Physics Impact Immunity',
-    image: iconProtectionPhysicsImpact,
+    image: getImagePath('protection_physics_impact.png'),
   },
   PROTECTION_POLYMORPH: {
     id: 'PROTECTION_POLYMORPH',
     name: 'Polymorph Immunity',
-    image: iconProtectionPolymorph,
+    image: getImagePath('protection_polymorph.png'),
   },
   PROTECTION_PROJECTILE: {
     id: 'PROTECTION_PROJECTILE',
     name: 'Projectile Immunity',
-    image: iconProtectionProjectile,
+    image: getImagePath('protection_projectile.png'),
   },
   PROTECTION_RADIOACTIVITY: {
     id: 'PROTECTION_RADIOACTIVITY',
     name: 'Toxic Immunity',
-    image: iconProtectionRadioActivity,
+    image: getImagePath('protection_radioactivity.png'),
   },
   PROTECTION_RESURRECTION: {
     id: 'PROTECTION_RESURRECTION',
     name: 'Resurrection Immunity',
-    image: iconProtectionResurrection,
+    image: getImagePath('protection_resurrection.png'),
   },
   PROTECTION_SUFFOCATE: {
     id: 'PROTECTION_SUFFOCATE',
     name: 'Suffocation Immunity',
-    image: iconProtectionSuffocate,
+    image: getImagePath('protection_suffocate.png'),
   },
   PROTECTION_TOUCH_MAGIC: {
     id: 'PROTECTION_TOUCH_MAGIC',
     name: 'Touch Magic Immunity',
-    image: iconProtectionTouchMagic,
+    image: getImagePath('protection_touch_of.png'),
   },
 } as const;
--- apps/web/src/noita/noita-spell-type-dictionary.ts
+++ apps/web/src/noita/noita-spell-type-dictionary.ts
@@ -1,47 +1,48 @@
 import { StringKeyDictionary } from '@noita-explorer/model';
 import { NoitaSpellType } from '@noita-explorer/model-noita';
 
-const bgBasePath = '/images/data-wak/spell-backgrounds/';
+const basePath = '/images/data-wak/spell-backgrounds/';
+const getImagePath = (file: string) => basePath + file;
 
 export const NoitaSpellTypesDictionary: StringKeyDictionary<NoitaSpellType> = {
   ACTION_TYPE_DRAW_MANY: {
     id: 'ACTION_TYPE_DRAW_MANY',
     name: 'Multicast',
-    image: bgBasePath + 'item_bg_draw_many.png',
+    image: getImagePath('item_bg_draw_many.png'),
   },
   ACTION_TYPE_MATERIAL: {
     id: 'ACTION_TYPE_MATERIAL',
     name: 'Material',
-    image: bgBasePath + 'item_bg_material.png',
+    image: getImagePath('item_bg_material.png'),
   },
   ACTION_TYPE_MODIFIER: {
     id: 'ACTION_TYPE_MODIFIER',
     name: 'Projectile Modifier',
-    image: bgBasePath + 'item_bg_modifier.png',
+    image: getImagePath('item_bg_modifier.png'),
   },
   ACTION_TYPE_OTHER: {
     id: 'ACTION_TYPE_OTHER',
     name: 'Other',
-    image: bgBasePath + 'item_bg_other.png',
+    image: getImagePath('item_bg_other.png'),
   },
   ACTION_TYPE_PASSIVE: {
     id: 'ACTION_TYPE_PASSIVE',
     name: 'Passive',
-    image: bgBasePath + 'item_bg_passive.png',
+    image: getImagePath('item_bg_passive.png'),
   },
   ACTION_TYPE_PROJECTILE: {
     id: 'ACTION_TYPE_PROJECTILE',
     name: 'Projectile',
-    image: bgBasePath + 'item_bg_projectile.png',
+    image: getImagePath('item_bg_projectile.png'),
   },
   ACTION_TYPE_STATIC_PROJECTILE: {
     id: 'ACTION_TYPE_STATIC_PROJECTILE',
     name: 'Static Projectile',
-    image: bgBasePath + 'item_bg_static_projectile.png',
+    image: getImagePath('item_bg_static_projectile.png'),
   },
   ACTION_TYPE_UTILITY: {
     id: 'ACTION_TYPE_UTILITY',
     name: 'Utility',
-    image: bgBasePath + 'item_bg_utility.png',
+    image: getImagePath('item_bg_utility.png'),
   },
 } as const;
