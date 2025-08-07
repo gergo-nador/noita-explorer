064fc097c6f15fa7c815e4073953c94f6e8f11d0
Gergo Nador <wotng2002@gmail.com>
2025-08-07 18:31:33 +0200

    set static locale

--- apps/web/src/pages/credits.tsx
+++ apps/web/src/pages/credits.tsx
@@ -223,7 +223,9 @@ export const Credits = () => {
       <Flex gap={10} center className='text-secondary'>
         <span>{__DEPLOY_ID__}</span>
         <span>-</span>
-        <span>{new Date(parseInt(__DEPLOY_TIME__)).toLocaleString()}</span>
+        <span>
+          {new Date(parseInt(__DEPLOY_TIME__)).toLocaleString('en-GB')}
+        </span>
       </Flex>
     </div>
   );
