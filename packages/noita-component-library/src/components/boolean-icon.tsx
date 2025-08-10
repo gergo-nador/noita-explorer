f1c9ac67c959895e6f81f1b348d1dec09b8b5edd
Gergo Nador <wotng2002@gmail.com>
2025-08-10 16:53:48 +0200

    small boolean-icon.tsx refactor

--- packages/noita-component-library/src/components/boolean-icon.tsx
+++ packages/noita-component-library/src/components/boolean-icon.tsx
@@ -6,8 +6,8 @@ interface BooleanIconProps {
 
 export const BooleanIcon = ({ value }: BooleanIconProps) => {
   return value ? (
-    <Icon type={'check'} alt={'Yes'} size={16} />
+    <Icon type='check' alt='Yes' size={16} />
   ) : (
-    <Icon type={'cross'} alt={'No'} size={16} />
+    <Icon type='cross' alt='No' size={16} />
   );
 };
