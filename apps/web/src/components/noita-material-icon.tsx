d368f0df611c982641857cb4a53a5c103ecf905c
Gergo Nador <wotng2002@gmail.com>
2025-08-16 02:14:52 +0200

    fix material icon

--- apps/web/src/components/noita-material-icon.tsx
+++ apps/web/src/components/noita-material-icon.tsx
@@ -1,61 +1,23 @@
 import { NoitaMaterial } from '@noita-explorer/model-noita';
 import { InventoryIcon } from '@noita-explorer/noita-component-library';
-import { useEffect, useState } from 'react';
 import { publicPaths } from '../utils/public-paths.ts';
-import {
-  renderMaterialPotion,
-  renderMaterialPouch,
-} from '../noita/noita-materials.ts';
+import { getMaterialIconType } from '../noita/noita-materials.ts';
 
 interface NoitaMaterialIconProps {
   material: NoitaMaterial;
 }
 
 export const NoitaMaterialIcon = ({ material }: NoitaMaterialIconProps) => {
-  const [materialIcon, setMaterialIcon] = useState<string | undefined>(() => {
-    const iconType = getMaterialIconType(material);
-
-    if (!iconType) {
-      return;
-    }
-
-    const iconCacheKey = getMaterialIconCacheKey(iconType, material.id);
-    const materialCache = getMaterialIconFromCache(iconCacheKey);
-    if (materialCache) {
-      return materialCache;
-    }
-  });
-
-  useEffect(() => {
-    const iconType = getMaterialIconType(material);
-
-    if (!iconType) {
-      return;
-    }
-
-    const iconCacheKey = getMaterialIconCacheKey(iconType, material.id);
-    const materialCache = getMaterialIconFromCache(iconCacheKey);
-    if (materialCache) {
-      setMaterialIcon(materialCache);
-      return;
-    }
-
-    let iconPromise: Promise<string>;
-
-    if (iconType === 'potion') {
-      iconPromise = renderMaterialPotion(material);
-    } else {
-      iconPromise = renderMaterialPouch(material);
-    }
-
-    iconPromise.then((icon) => {
-      setMaterialIcon(icon);
-      sessionStorage[iconCacheKey] = icon;
-    });
-  }, [material]);
-
-  if (materialIcon) {
-    return <InventoryIcon icon={materialIcon} />;
+  const materialContainmentType = getMaterialIconType(material);
+  if (materialContainmentType) {
+    return (
+      <InventoryIcon
+        icon={publicPaths.generated.material.image({
+          materialId: material.id,
+          type: materialContainmentType,
+        })}
+      />
+    );
   }
 
   if (material.hasGraphicsImage) {
@@ -83,26 +45,4 @@ export const NoitaMaterialIcon = ({ material }: NoitaMaterialIconProps) => {
       }}
     ></div>
   );
-};
-
-const getMaterialIconType = (
-  material: NoitaMaterial,
-): 'pouch' | 'potion' | undefined => {
-  if (material.cellType === 'liquid' && !material.liquidSand) {
-    return 'potion';
-  } else if (material.cellType === 'liquid' && material.liquidSand) {
-    return 'pouch';
-  } else return;
-};
-
-const getMaterialIconCacheKey = (iconType: string, materialId: string) =>
-  `${iconType}-${materialId}`;
-
-const getMaterialIconFromCache = (cacheId: string): string | undefined => {
-  const cache = sessionStorage;
-
-  if (cacheId in cache) {
-    const icon = cache[cacheId];
-    return icon;
-  }
 };
