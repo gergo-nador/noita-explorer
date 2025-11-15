4ed9ff49516891e5e6e2eb4c988c07f20be49f66
Gergo Nador <wotng2002@gmail.com>
2025-11-15 13:06:42 +0100

    update worker loaders

--- apps/web/src/pages/map/components/loaders/workers-loader.tsx
+++ apps/web/src/pages/map/components/loaders/workers-loader.tsx
@@ -3,26 +3,31 @@ import { useThreadsPool } from '../../map-renderer-threads/use-threads-pool.ts';
 export const WorkersLoader = () => {
   const { status } = useThreadsPool();
 
-  return Object.values(status).map((status) => {
-    return (
-      <div>
-        <span>{status.id}: </span>
-        {status.state === 'started' && (
-          <span style={{ color: 'grey' }}>{status.state}</span>
-        )}
-        {status.state === 'initialized' && (
-          <span style={{ color: 'yellow' }}>{status.state}</span>
-        )}
-        {status.state === 'running' && (
-          <span style={{ color: 'green' }}>{status.state}</span>
-        )}
-        {status.state === 'error' && (
-          <span style={{ color: 'red' }}>{status.state}</span>
-        )}
-        {status.state === 'init-error' && (
-          <span style={{ color: 'red' }}>{status.state}</span>
-        )}
-      </div>
-    );
-  });
+  return (
+    <div>
+      <div>Workers:</div>
+      {Object.values(status).map((status) => {
+        return (
+          <div>
+            <span>{status.id}: </span>
+            {status.state === 'started' && (
+              <span style={{ color: 'grey' }}>{status.state}</span>
+            )}
+            {status.state === 'initialized' && (
+              <span style={{ color: 'yellow' }}>{status.state}</span>
+            )}
+            {status.state === 'running' && (
+              <span style={{ color: 'green' }}>{status.state}</span>
+            )}
+            {status.state === 'error' && (
+              <span style={{ color: 'red' }}>{status.state}</span>
+            )}
+            {status.state === 'init-error' && (
+              <span style={{ color: 'red' }}>{status.state}</span>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
